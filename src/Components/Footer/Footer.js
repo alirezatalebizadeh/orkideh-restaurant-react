ccceba0d1d2ea7571779d4c1daffaa0ffa74de4f
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-24 06:35:09 +0330

    create footer and navbar

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,57 +1,98 @@
-import React from 'react'
-import './Footer.ss'
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaFacebookSquare } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Footer.css";
 export default function Footer() {
-    return (
-        <>
-            <footer class="footer-area bg-f">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-3 col-md-6">
-                            <h3 class="text-right">اشتراک </h3>
-                            <div class="subscribe_form text-right" dir="rtl">
-                                <form class="subscribe_form">
-                                    <input name="EMAIL" id="subs-email" class="form_input" placeholder="ادرس ایمیل شما" type="email" />
-                                    <button type="submit" class="submit">اشتراک</button>
-                                    <div class="clearfix"></div>
-                                </form>
-                            </div>
-                            <ul class="list-inline f-social">
-                                <li class="list-inline-item"><a class="facebook" href="https://www.facebook.com/ORRestaurant/"></a></li>
-                                <li class="list-inline-item"><a class="linkdin" href="https://www.linkedin.com/in/orkideh-restaurant-822328140"></a></li>
-                                <li class="list-inline-item"><a class="insta" href="https://www.instagram.com/orkideh_restaurant/"></a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-3 col-md-6 text-right">
-                            <h3 class="text-right">شعبه ها</h3>
-                            <p class="lead"> سعادت اباد-جاده چالوش</p>
-                            <p class="lead"> اقدسیه-متل قو</p>
-                            <p>ارژانتین-مهستان-شهریار</p>
-                        </div>
-                        <div class="col-lg-3 col-md-6 text-right">
-                            <h3>ساعات کار</h3>
-                            <p><span class="text-color">شنبه-یک شنبه :</span> 12:00 - 24:00 </p>
-                            <p><span class="text-color">دو شنبه-سه شنبه : </span>12:00 - 24:00 </p>
-                            <p><span class="text-color">چهار شنبه-پنج شنبه :</span> 12:00 - 24:00 </p>
-                            <p><span class="text-color">جمعه :</span> 12:00 - 24:00 </p>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <h3 class="text-right">درباره ما</h3>
-                            <p class="text-right">رستوران‌های زنجیره‌ای ارکیده با بیش از ۶۵ سال تجربه، افتخار آن را دارد که در ۸ شعبه در سراسر کشور، میزبان محافل دوستانه و کاری، جشن‌ها و مراسم‌های شما عزیزان باشد.</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className="footer-area bg-f">
+      <div className="container">
+        <Row>
+          <Col md={6} lg={3}>
+            <h3 className="text-right">اشتراک </h3>
+            <div className="subscribe_form text-right" dir="rtl">
+              <form className="subscribe_form">
+                <input
+                  name="EMAIL"
+                  id="subs-email"
+                  className="form_input"
+                  placeholder="ادرس ایمیل شما"
+                  type="email"
+                />
+                <button type="submit" className="submit">
+                  اشتراک
+                </button>
+                <div className="clearfix"></div>
+              </form>
+            </div>
+            <ul className="list-inline f-social">
+              <li className="list-inline-item">
+                <a
+                  className="facebook"
+                  href="https://www.facebook.com/ORRestaurant/"
+                ></a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  className="linkdin"
+                  href="https://www.linkedin.com/in/orkideh-restaurant-822328140"
+                ></a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  className="insta"
+                  href="https://www.instagram.com/orkideh_restaurant/"
+                ></a>
+              </li>
+            </ul>
+          </Col>
+          <Col md={6} lg={3} className="text-right">
+            <h3 className="text-right">شعبه ها</h3>
+            <p className="lead"> سعادت اباد-جاده چالوش</p>
+            <p className="lead"> اقدسیه-متل قو</p>
+            <p>ارژانتین-مهستان-شهریار</p>
+          </Col>
+          <Col md={6} lg={3} className="text-right">
+            <h3>ساعات کار</h3>
+            <p>
+              <span className="text-color">شنبه-یک شنبه :</span> 12:00 - 24:00{" "}
+            </p>
+            <p>
+              <span className="text-color">دو شنبه-سه شنبه : </span>12:00 -
+              24:00{" "}
+            </p>
+            <p>
+              <span className="text-color">چهار شنبه-پنج شنبه :</span> 12:00 -
+              24:00{" "}
+            </p>
+            <p>
+              <span className="text-color">جمعه :</span> 12:00 - 24:00{" "}
+            </p>
+          </Col>
+          <Col md={6} lg={3}>
+            <h3 className="text-right">درباره ما</h3>
+            <p className="text-right">
+              رستوران‌های زنجیره‌ای ارکیده با بیش از ۶۵ سال تجربه، افتخار آن را
+              دارد که در ۸ شعبه در سراسر کشور، میزبان محافل دوستانه و کاری،
+              جشن‌ها و مراسم‌های شما عزیزان باشد.
+            </p>
+          </Col>
+        </Row>
+      </div>
 
-                <div class="copyright">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <p class="company-name">تمامی حقوق مادی و معنوی سایت متعلق به رستوران ارکیده می باشد</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </footer>
-        </>
-    )
+      <div className="copyright">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <p className="company-name">
+                تمامی حقوق مادی و معنوی سایت متعلق به رستوران ارکیده می باشد
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  Offcanvas,
+  NavDropdown,
+} from "react-bootstrap";
 
 export default function Header() {
-    return (
-        <div>
-            home
-        </div>
-    )
+  return (
+    <Navbar expand={false} className="bg-body-tertiary mb-3">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${false}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
+              Offcanvas
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown
+                title="Dropdown"
+                id={`offcanvasNavbarDropdown-expand-${false}`}
+              >
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
 }
