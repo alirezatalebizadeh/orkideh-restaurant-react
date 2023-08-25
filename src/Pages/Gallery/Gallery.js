2cd81f02c71831391df2a8be1669c72a2fbeed61
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-25 17:42:39 +0330

    debug gallery page

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,13 +1,14 @@
-import React from "react";
-import GalleryHover from "../../Components/GalleryHover/GalleryHover";
-import GallerySlider from "../../Components/GallerySlider/GallerySlider";
+import React from 'react'
+import GalleryHover from '../../Components/GalleryHover/GalleryHover'
+import GallerySlider from '../../Components/GallerySlider/GallerySlider'
 import './Gallery.css'
 
-
-export default function Gallery() {
-  return <div className="gallery">
-    <GallerySlider />
-    <h2 className="title">گالری ما </h2>
-    <GalleryHover />
-  </div>;
+export default function Gallery () {
+  return (
+    <div className='gallery'>
+      <GallerySlider />
+      <h2 className='title'>گالری ما </h2>
+      <GalleryHover />
+    </div>
+  )
 }
